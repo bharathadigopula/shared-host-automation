d1d903eed4bd4620a66d9d39b62134df0050e637
bharathadigopula <48708941+bharathadigopula@users.noreply.github.com>
2026-09-01 12:24:20 +0530

    feat: publish Jenkins validation status (#11)

--- .jenkins/pipelines/validate.groovy
+++ .jenkins/pipelines/validate.groovy
@@ -2,9 +2,10 @@
 // SHARED HOST AUTOMATION VALIDATION
 //==============================================================================
 
-@Library('jenkins-pipeline-templates@v1.3.0') _
+@Library('jenkins-pipeline-templates@v1.4.0') _
 
 repositoryValidationPipeline(
+    githubRepository: 'bharathadigopula/shared-host-automation',
     shellSearchPath: 'scripts',
     validationScript: 'scripts/validate.sh',
     validateWorkflows: true
